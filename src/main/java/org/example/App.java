ccd2d729b2f12b9bc3c0c41647e1fba8bbdde3fb
MPJ <michaelphelps40384@gmail.com>
2023-01-04 23:32:41 +0100

    Subida 4.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,35 @@
 package org.example;
 
+import java.util.function.*;
+import java.util.stream.IntStream;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        if (args.length == 0) return;
+        else {
+            int n = Integer.parseInt(args[0]);
+            int fact = IntStream.range(1, n).reduce(1, (i, i1) -> i*i1);
+            int[] fibo = IntStream.range(1, n).collect(
+                    (Supplier<int[]>) () -> {
+                        int[] arr = new int[2];
+                        arr[1] = 1;
+                        return arr;
+                    },
+                    (ObjIntConsumer<int[]>) (ints, i) -> {
+                        var a = ints[0];
+                        var b = ints[1];
+                        ints[0] = b;
+                        ints[1] = a+b;
+                    },
+                    (ints, ints2) -> {}
+            );
+            System.out.println("El factorial de " + n + " es " + fact +"." );
+            System.out.println("El número fibonacci " + n + "º es " + fibo[1] +"." );
+        }
     }
 }
